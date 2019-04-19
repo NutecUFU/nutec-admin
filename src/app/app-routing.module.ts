6933ce9809e252524f18bc9b0f1f7fa72b548bbd
matheussn <matheusneto23@gmail.com>
2019-04-19 19:31:39 -0300

    iniciando template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
+  { path: 'login', loadChildren: './modules/login/login.module#LoginModule' },
   { path: '**', loadChildren: './modules/not-found/not-found.module#NotFoundModule' }
 ];
 
