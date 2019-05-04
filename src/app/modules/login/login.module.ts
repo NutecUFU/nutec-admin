26a772ad3233c8a3d8b823f576df1bf3a4a9838f
matheussn <matheusneto23@gmail.com>
2019-05-04 16:08:53 -0300

    Alterando layout

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routing';
+import { AdminCommonModule } from 'src/app/common/admin-common.module';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    AdminCommonModule
   ]
 })
 export class LoginModule { }
