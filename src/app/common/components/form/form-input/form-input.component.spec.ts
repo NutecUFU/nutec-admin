e11852b8a130e3f35bc95d5b23c8c75c3d7c2bcf
Matheus <matheusneto23@gmail.com>
2019-06-16 19:27:00 -0300

    fix test

--- src/app/common/components/form/form-input/form-input.component.spec.ts
+++ src/app/common/components/form/form-input/form-input.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminCommonModule } from './../../../admin-common.module';
 import { FormInputComponent } from './form-input.component';
@@ -6,11 +7,20 @@ import { FormInputComponent } from './form-input.component';
 describe('FormInputComponent', () => {
   let component: FormInputComponent;
   let fixture: ComponentFixture<FormInputComponent>;
+  const formControlName = 'name';
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [],
-      imports: [AdminCommonModule]
+      imports: [
+        AdminCommonModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilder }
+      ]
     })
     .compileComponents();
   }));
@@ -18,6 +28,10 @@ describe('FormInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormInputComponent);
     component = fixture.componentInstance;
+    component.name = formControlName;
+    component.formGroup = formBuilder.group({
+      [formControlName]: null
+    });
     fixture.detectChanges();
   });
 
--- src/app/modules/login/login.component.spec.ts
+++ src/app/modules/login/login.component.spec.ts
@@ -1,9 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { AdminCommonModule } from 'src/app/common/admin-common.module';
 
 import { FakeLoader } from './../../common/utils/unit-tests/FakeLoader';
 import { LoginComponent } from './login.component';
+import { LoginService } from './services/login.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -14,10 +18,15 @@ describe('LoginComponent', () => {
       declarations: [ LoginComponent ],
       imports: [
         AdminCommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule,
         TranslateModule.forRoot({
           loader: { provide: TranslateLoader, useClass: FakeLoader }
         })
-      ]
+      ],
+      providers: [LoginService]
     })
     .compileComponents();
   }));
