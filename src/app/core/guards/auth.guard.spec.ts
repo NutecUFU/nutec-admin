03a0678c8c7c784f57fae6de9e15f094f68d1189
Matheus <matheusneto23@gmail.com>
2019-06-19 20:16:08 -0300

    fix tests

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,11 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { inject, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { AuthService } from '../services/auth.service';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AuthGuard,
+        AuthService
+      ]
     });
   });
 
