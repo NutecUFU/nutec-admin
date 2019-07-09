bed2150087a5967ffa2a5ef4e7fea94bdcf01106
Matheus <matheusneto23@gmail.com>
2019-07-09 11:25:39 -0300

    fix tests

--- src/app/modules/main/main.component.spec.ts
+++ src/app/modules/main/main.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
@@ -8,7 +10,8 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ MainComponent, SideBarComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
